06ab6735939114a2d13b195e0f476d29ab402d02
Loren Davie <code@axilent.com>
2016-07-05 09:55:34 -0400

    Fleshing out setup

--- setup.py
+++ setup.py
@@ -17,9 +17,17 @@ setup(
     url='http://github.com/Axilent/talkback',
     install_requires=[
         'Flask>=0.11.1',
+        'Jinja2>=2.8',
+        'MarkupSafe>=0.23',
+        'PyYAML>=3.11',
+        'Werkzeug>=0.11.10',
+        'click>=6.6',
+        'itsdangerous>=0.24',
         'nltk>=3.2.1',
         'requests>=2.10.0',
-        'textblob>=0.11.1'],
+        'textblob>=0.11.1',
+        'wsgiref==0.1.2',
+    ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
